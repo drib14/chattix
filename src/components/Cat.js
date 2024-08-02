2108d9acd217b2f8878266c5f945945641a53340
drib14 <jhondribramirez7@gmail.com>
2024-08-02 21:55:40 +0800

    little update

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+
+const Cat = () => {
+    return (
+        <Avatar src='https://images.pexels.com/photos/1276553/pexels-photo-1276553.jpeg?auto=compress&cs=tinysrcgb&w=1260&h=750&dpr=2'/>
+    )
+}
+
+export default Cat
